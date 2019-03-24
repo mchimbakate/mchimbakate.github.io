70ba63dab354508b74974bc53594873526497549
Tatenda Chimbakate <mchimbakate@gmail.com>
2019-03-24 23:36:16 +0000

    Completed Asthma page

---
+++ getxml.js
@@ -0,0 +1,27 @@
+function getXML(){
+var xhttp;
+xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+    }
+};
+xhttp.open("GET", "asthma.xml", false);
+xhttp.withCredentials = true;
+xhttp.send();
+
+function myFunction(xml) {
+    var x, i, txt, xmlDoc; 
+    xmlDoc = xml.responseXML;
+    txt = "";
+    x = xmlDoc.getElementsByTagName("title");
+    for (i = 0; i < x.length; i++) { 
+        txt += "<td>" + x[i].childNodes[0].nodeValue+"</td>";
+        Console.log(txt);
+    }
+    document.getElementById("orgChartData").innerHTML =txt +"</tr></table></div>";
+    
+}
+}
+
+       
